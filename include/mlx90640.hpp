3d3ccccaa53deae551929ff47db27f5522a553a5
EuphoricCatface <euphoriccatface@gmail.com>
2021-04-17 22:12:59 +0900

    main loop logic into mlx90640 class

--- include/mlx90640.hpp
+++ include/mlx90640.hpp
@@ -183,7 +183,26 @@ public:
 		VDD_raw = ram.named.VDD_raw;
 		V_PTAT = ram.named.Ta_PTAT; // p18 says Ta_PTAT but p23 says V_PTAT
 		V_BE = ram.named.V_BE;
+		printf("VDD_raw is %04hX\n", VDD_raw);
+		printf("V_BE, V_PTAT: %hd, %hd\n", V_BE, V_PTAT);
+
 		return true;
+	}
+
+private:
+	double dV;
+	double V_PTAT_art;
+	double dTa;
+
+public:
+	void process_data(){
+		dV = (double)(-((int)Vdd_25_EE << 5) + VDD_raw + 16384) / (double) K_Vdd_EE / 32.0;
+	    V_PTAT_art = (double)(1 << 18) / (a_PTAT + (double)V_BE / (double)V_PTAT);
+	    dTa = (V_PTAT_art / (1.0 + K_V_PTAT * dV) - V_PTAT_25) / K_T_PTAT;
+
+	    printf("dV: %lf\n", dV);
+	    printf("V_PTAT_art is %lf\n", V_PTAT_art);
+	    printf("dTa: %lf\n", dTa);
 	}
 };
 
--- src/main.cpp
+++ src/main.cpp
@@ -13,10 +13,10 @@ int main() {
 	std::cout << "Hello World\n";
 
 	mlx90640 device = mlx90640();
-	//device.init_ee("/sys/bus/i2c/devices/1-0033/nvmem/nvram");
 	device.init_ee("/home/USER/mlx/nvmem");
-	device.print_ee();
+	device.init_frame_file("/home/USER/mlx/mlx_v4l2_raw_frames");
 
+	//device.init_ee("/sys/bus/i2c/devices/1-0033/nvmem/nvram");
 	// printf("EE[2433] is %04hX\n", nv.fetch_EE_address(0x2433));
 	unsigned int Vdd_25_EE = device.get_Vdd25_EE();
 	int K_Vdd_EE = device.get_K_Vdd_EE();
@@ -29,25 +29,11 @@ int main() {
 	// printf("EE[2432] is %04hX\n", nv.fetch_EE_address(0x2432));
 	printf("K_V_PTAT is %lf, K_T_PTAT is %lf, V_PTAT_25 is %hd\n", K_V_PTAT, K_T_PTAT, V_PTAT_25);
 
-	device.init_frame_file("/home/USER/mlx/filesinkdump");
-
 	while(1){
 	    if(!device.process_frame_file())
 	        break;
 
-	    int16_t dV_raw = device.get_VDD_raw();
-	    printf("dV_raw is %04hX, ", dV_raw);
-	    double dV = (double)(-((int)Vdd_25_EE << 5) + dV_raw + 16384) / (double) K_Vdd_EE / 32.0;
-	    printf("dV: %lf\n", dV);
-
-	    int16_t V_BE = device.get_V_BE();
-	    int16_t V_PTAT = device.get_V_PTAT();
-
-	    double V_PTAT_art = (double)(1 << 18) / (a_PTAT + (double)V_BE / (double)V_PTAT);
-	    printf("V_BE, V_PTAT, V_PTAT_art: %hd, %hd, %lf\n", V_BE, V_PTAT, V_PTAT_art);
-
-	    double dTa = (V_PTAT_art / (1.0 + K_V_PTAT * dV) - V_PTAT_25) / K_T_PTAT;
-	    printf("dTa: %lf\n", dTa);
+	    device.process_data();
 	}
 
     printf("closing\n");
