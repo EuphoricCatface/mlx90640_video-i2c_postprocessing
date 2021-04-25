952ac195a49b0f4d2bdcf069ed060841c7e91c06
EuphoricCatface <euphoriccatface@gmail.com>
2021-04-25 10:47:54 +0900

    fixup: status register

--- src/mlx90640.cpp
+++ src/mlx90640.cpp
@@ -9,16 +9,22 @@ void drv_adapter_init(mlx90640 * ptr){
 }
 
 int MLX90640_I2CRead(uint8_t slaveAddr,uint16_t startAddress, uint16_t nMemAddressRead, uint16_t *data){
+    static int data_avail = 0;
+    static int subpg = 0;
 	switch(startAddress){
 	case 0x400:
 		memcpy(data, adapter_ptr->ram.word_, nMemAddressRead * 2);
+		subpg++;
 		break;
 	case 0x2400:
 		memcpy(data, adapter_ptr->ee.word_, nMemAddressRead * 2);
 		break;
 	case 0x8000:
-		*data = 0x0008 // Pretend data is always available
-				| 0x0001; // Subpage does not matter if TGC is 0
+		*data = (data_avail%2 ? 0x0008 : 0) | // Pretend data is <s>always</s> available
+		                // actually, main logic cannot proceed if this is stuck
+				(subpg%2 ? 0x0001 : 0); // <s>Subpage does not matter if TGC is 0</s>
+				        // actually, the main logic tries to calculate only corresponding subpage
+		data_avail++;
 		break;
 	case 0x800D:
 		*data = 0x1000 // Chess pattern
